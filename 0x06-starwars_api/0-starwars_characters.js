a61fd19b6f474b122e927f6e18004d6aa754b509
Ken Wekesa <wanyamak884@gmail.com>
2025-08-08 11:59:46 +0300

    Star wars characters

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+const request = require('request');
+
+const movieID = process.argv[2];
+
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
+
+request(apiUrl, (err, response, body) => {
+  if (err) {
+    console.error(err);
+  }
+  const characters = JSON.parse(body).characters;
+  characters.forEach((char) => {
+    request(char, (err, response, body) => {
+      if (err) {
+        console.error(err);
+      }
+      console.log(JSON.parse(body).name);
+    });
+  });
+});
