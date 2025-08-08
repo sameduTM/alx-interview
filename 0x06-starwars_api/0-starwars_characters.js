091a9b04e450532cbfbb014174295b7af2df3fac
Ken Wekesa <wanyamak884@gmail.com>
2025-08-08 13:05:02 +0300

    write all characters in order

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,21 +1,22 @@
 #!/usr/bin/node
-const request = require('request');
+const rp = require('request-promise');
 
 const movieID = process.argv[2];
 
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
 
-request(apiUrl, (err, response, body) => {
-  if (err) {
-    console.error(err);
-  }
-  const characters = JSON.parse(body).characters;
-  characters.forEach((char) => {
-    request(char, (err, response, body) => {
-      if (err) {
-        console.error(err);
+rp(apiUrl)
+  .then(function (resp) {
+    const characters = JSON.parse(resp).characters;
+    async function getCharacter () {
+      for (let i = 0; i < characters.length; i++) {
+        const char = await rp(characters[i]);
+        console.log(JSON.parse(char).name);
       }
-      console.log(JSON.parse(body).name);
-    });
+      console.log('Done!');
+    }
+    getCharacter();
+  })
+  .catch(function (err) {
+    console.error(err);
   });
-});
